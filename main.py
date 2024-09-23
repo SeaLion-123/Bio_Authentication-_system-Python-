aab0745bc40d19d84b41575d85d16349a1385ee7
Silas Manku(SeaLion) <113685069+SeaLion-123@users.noreply.github.com>
2024-09-23 14:22:51 +0200

    The program can now detect faces.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # To work with open cv need to import it.
 import cv2
-
 # will start off with the basics, loading an image.
-image = cv2.imread('C:\Users\Silas\Desktop')
-
+image = cv2.imread('C:\Users\Silas\Desktop\Multiple_faces.JPG',)
+#This is a gray scale vision of the image.
+gray_scale = cv2.imread('/home/wtc/Downloads/1722328614790.jpeg', cv2.IMREAD_GRAYSCALE)
 # checking if image was loaded successfully.
-
 if image is None:
     print("Error: Image not tholakaling!")
 else:
-    print("Image was loaded successfully !")
+    # print("Image was loaded successfully !")
+    # cv2.imshow("Image Wndow", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    #A model detection that comes pre-trained.
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray_scale, scaleFactor=1.1, minSize=(30,30))
+    for (x,y,w,h) in faces:
+        cv2.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
+        cv2.imwrite('detected_faces.jpg',image)
+        cv2.imshow('Face Detection', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
